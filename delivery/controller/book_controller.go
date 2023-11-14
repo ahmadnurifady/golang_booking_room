98dbd3d1a5ebe527c74b966e7d152bc29c878c94
Ricky Andriansyah <idrickyandriansyah@gmail.com>
2023-11-14 16:37:53 +0700

    [add] Menambahkan booking controller

---
+++ delivery/controller/book_controller.go
@@ -0,0 +1,61 @@
+package controller
+
+import (
+	"final-project-booking-room/model/dto"
+	"final-project-booking-room/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BookingController struct {
+	uc usecase.BookingUseCase
+	rg *gin.RouterGroup
+}
+
+func (b *BookingController) createHandler(ctx *gin.Context) {
+	var payload dto.BookingRequestDto
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	rspPayload, err := b.uc.RegisterNewBooking(payload)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	common.SendCreateResponse(ctx, "Ok", rspPayload)
+}
+
+func (b *BookingController) getHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		common.SendErrorResponse(ctx, http.StatusBadRequest, "Booking ID can't be empty")
+		return
+	}
+
+	rspPayload, err := b.uc.FindById(id)
+	if err != nil {
+		common.SendErrorResponse(ctx, http.StatusNotFound, err.Error())
+		return
+	}
+
+	common.SendSingleResponse(ctx, "Ok", rspPayload)
+}
+
+func (b *BookingController) Route() {
+	br := b.rg.Group("/booking")
+	br.POST("/", b.createHandler)
+	br.GET("/:id", b.getHandler)
+}
+
+func NewBookingController(
+	uc usecase.BookingUseCase,
+	rg *gin.RouterGroup,
+) *BookingController {
+	return &BookingController{
+		uc: uc,
+		rg: rg}
+}
