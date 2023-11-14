6d9eb2734a21d0f90e6a930efa4550e8e872cf3a
dtsatria <dtresnasatria@gmail.com>
2023-11-14 09:41:42 -0800

    [ADD] DATABASE

--- assets/database.sql
+++ assets/database.sql
@@ -1,53 +1,58 @@
 CREATE DATABASE booking_room;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-//post,get,getbyid,update
-CREATE TABLE transaksi(
-    id          UUID,
-    userId      string,     //Foreign key ke user id
-    roomType    string,        //Foreign key ke room id
-    cek_in      time.time,
-    cek_out     time.time,
-    description string,
-    status      string,     //pending, decline atau accept
-)
-
-//post,getbyid,update,delete
-CREATE TABLE user(
-    id          UUID,
-    name        string,
-    divisi      string,
-    jabatan     string,         //manager, direktur itu di set di jabatan dan rolenya adalah admin
-    email       string,
-    password    string,
-    role        string,         //admin, karyawan atau GA
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100),
+    divisi VARCHAR(100),
+    jabatan VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100),
+    role VARCHAR(100),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP
 );
 
-//post,getbyid,getall,update
-CREATE TABLE Room(
-    id UUID,
-	RoomType  string,       
-	Capacity  string,      
-	Facility  int,            //Foreign key ke Facility id
-	Status    string,        //untuk status hanya ada dua yaitu Available atau Booked
-	CreatedAt time.Time,    
-	UpdatedAt time.Time,    
+CREATE TABLE facilities (
+    Id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    RoomDescription TEXT,
+    Fwifi VARCHAR(100),
+    FsoundSystem VARCHAR(100),
+    Fprojector VARCHAR(100),
+    FscreenProjector VARCHAR(100),
+    Fchairs VARCHAR(100),
+    Ftables VARCHAR(100),
+    FsoundProof VARCHAR(100),
+    FsmonkingArea VARCHAR(100),
+    Ftelevison VARCHAR(100),
+    FAc VARCHAR(100),
+    Fbathroom VARCHAR(100),
+    FcoffeMaker VARCHAR(100),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP
 );
 
-CREATE TABLE facility(
-    Id               string,    
-	RoomDescription  string,    
-	Fwifi            string,      
-	FsoundSystem     string,      
-	Fprojector       string,      
-	FscreenProjector string,      
-	Fchairs          string,      
-	Ftables          string,      
-	FsoundProof      string,      
-	FsmonkingArea    string,      
-	Ftelevison       string,      
-	FAc              string,
-    Fbathroom        string,      
-	FcoffeMaker      string,      
-	CreatedAt        time.Time, 
-	UpdatedAt        time.Time, 
+CREATE TABLE rooms (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    RoomType UUID,
+    Capacity int,
+    Facilities UUID,
+    Status VARCHAR(100),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP,
+    CONSTRAINT FK_facility FOREIGN KEY(Facilities) REFERENCES facilities(id)
+);
+
+CREATE TABLE booking (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    userId UUID,
+    roomType UUID,
+    bookingDateStart TIMESTAMP,
+    bookingDateEnd TIMESTAMP,
+    description TEXT,
+    status VARCHAR(100),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP,
+    CONSTRAINT FK_userId FOREIGN KEY(userId) REFERENCES users(id),
+    CONSTRAINT FK_roomType FOREIGN KEY(roomType) REFERENCES rooms(id)
 );
