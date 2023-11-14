4305952bb0c56cca7cc3d897ee2404dbc65fe714
dtsatria <dtresnasatria@gmail.com>
2023-11-14 09:44:17 -0800

    DB

--- model/booking.go
+++ model/booking.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Booking struct {
-	Id          string    `json:"bookingId"`
-	UserId      User      `json:"employe"`
-	RoomType    Room      `json:"RoomType"`
-	CheckIn     time.Time `json:"checkIn"`
-	Checkout    time.Time `json:"checkOut"`
-	Status      string    `json:"status"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	Id               string    `json:"bookingId"`
+	UserId           User      `json:"employe"`
+	RoomType         Room      `json:"RoomType"`
+	BookingDateStart time.Time `json:"checkIn"`
+	BookingDateEnd   time.Time `json:"checkOut"`
+	Description      string    `json:"description"`
+	Status           string    `json:"status"`
+	CreatedAt        time.Time `json:"createdAt"`
+	UpdatedAt        time.Time `json:"updatedAt"`
 }
 
 //ketika GA acc form booking, kolom status di tabel room otomatis berubah menjadi book
