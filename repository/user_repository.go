077d3441c107f8b361f2e989f2d3944365ea9936
dtsatria <dtresnasatria@gmail.com>
2023-11-15 11:29:19 -0800

    [ADD] GET ALL,UPDATE AND DELETE USER

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -10,6 +10,9 @@ import (
 type UserRepository interface {
 	GetById(id string) (model.User, error)
 	Create(payload model.User) (model.User, error)
+	UpdateUserById(id string, payload model.User) (model.User, error)
+	DeleteUserById(id string) (model.User, error)
+	GetAllUser() ([]model.User, error)
 }
 
 type userRepository struct {
@@ -41,6 +44,56 @@ func (u *userRepository) Create(payload model.User) (model.User, error) {
 	}
 
 	return user, nil
+}
+
+// !MENGUPDATE USER BERDASARKAN ID
+func (u *userRepository) UpdateUserById(id string, payload model.User) (model.User, error) {
+	var user model.User
+	err := u.db.QueryRow(common.UpdateUser, payload.Name, payload.Divisi, payload.Jabatan,
+		payload.Email, payload.Password, payload.Role, time.Now(), id).
+		Scan(&user.Id, &user.Name, &user.Divisi, &user.Jabatan, &user.Email, &user.Role, &user.UpdatedAt)
+
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
+}
+
+// !MENGHAPUS USER BERDASARKAN ID
+func (u *userRepository) DeleteUserById(id string) (model.User, error) {
+	_, err := u.db.Exec(common.DeleteUser, id)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return model.User{}, nil
+}
+
+// !MENAMPILKAN SEMUA USER
+func (u *userRepository) GetAllUser() ([]model.User, error) {
+	var users []model.User
+
+	rows, err := u.db.Query(common.GetAllUser)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user model.User
+		err := rows.Scan(&user.Id, &user.Name, &user.Divisi, &user.Jabatan, &user.Email, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
 
 func NewUserRepository(db *sql.DB) UserRepository {
--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -11,10 +11,35 @@ import (
 type UserUseCase interface {
 	FindById(id string) (model.User, error)
 	RegisterNewUser(payload model.User) (model.User, error)
+	DeleteUser(id string) (model.User, error)
+	ViewAllUser() ([]model.User, error)
+	UpdateUserById(id string, payload model.User) (model.User, error)
 }
 
 type userUseCase struct {
 	repo repository.UserRepository
+}
+
+// UpdateUserById implements UserUseCase.
+func (u *userUseCase) UpdateUserById(id string, payload model.User) (model.User, error) {
+	if !payload.IsValidRole() {
+		return model.User{}, errors.New("invalid role, role must admin or employee")
+	}
+	newPassword, err := common.GeneratePasswordHash(payload.Password)
+	if err != nil {
+		return model.User{}, err
+	}
+	payload.Password = newPassword
+	return u.repo.UpdateUserById(id, payload)
+}
+
+// ViewAllUser implements UserUseCase.
+func (u *userUseCase) ViewAllUser() ([]model.User, error) {
+	user, err := u.repo.GetAllUser()
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 func (u *userUseCase) FindById(id string) (model.User, error) {
@@ -23,6 +48,14 @@ func (u *userUseCase) FindById(id string) (model.User, error) {
 		return model.User{}, fmt.Errorf("user with ID %s not found", id)
 	}
 	return user, nil
+}
+
+func (u *userUseCase) DeleteUser(id string) (model.User, error) {
+	_, err := u.repo.DeleteUserById(id)
+	if err != nil {
+		return model.User{}, fmt.Errorf("user with ID %s not found", id)
+	}
+	return model.User{}, nil
 }
 
 func (u *userUseCase) RegisterNewUser(payload model.User) (model.User, error) {
