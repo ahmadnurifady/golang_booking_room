a00c372d3d34ed6ea8c0fed0b61f2d3c4d3646da
Ahmad Nur Faizi <ahmadnurfaizi121@gmail.com>
2023-11-22 06:27:52 +0700

    continue unit testing

--- repository/room_repository_test.go
+++ repository/room_repository_test.go
@@ -156,69 +156,236 @@ func (suite *RoomRepositoryTestSuite) TestGetByRoomType() {
 
 	result, err := suite.repo.GetByRoomType("room test")
 
-	// assert.NoError(suite.T(), err)
-	// assert.Equal(suite.T(), mockRoom, result)
-	// assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
-
 	assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
 	assert.Nil(suite.T(), err)
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), mockRoom, result)
 
-	// suite.mockSql.ExpectQuery("^SELECT .*").WithArgs("roomtype_value").WillReturnRows(sqlmock.NewRows(
-	// 	[]string{"id", "roomtype", "capacity", "facilitie.id", "facilitie.roomdescription", "facilitie.fwifi", "facilitie.fsoundsystem", "facilitie.fprojector", "facilitie.fscreenprojector", "facilitie.fchairs", "facilitie.ftables", "facilitie.fsoundproof", "facilitie.fsmonkingarea", "facilitie.ftelevison", "facilitie.fac", "facilitie.fbathroom", "facilitie.fcoffemaker", "facilitie.createdat", "facilitie.updatedat", "status", "createdat", "updatedat"}).AddRow(mockRoom.Id, mockRoom.RoomType, mockRoom.MaxCapacity,
-	// 	mockRoom.Facility.Id, mockRoom.Facility.RoomDescription, mockRoom.Facility.Fwifi,
-	// 	mockRoom.Facility.FsoundSystem, mockRoom.Facility.Fprojector, mockRoom.Facility.FscreenProjector,
-	// 	mockRoom.Facility.Fchairs, mockRoom.Facility.Ftables, mockRoom.Facility.FsoundProof,
-	// 	mockRoom.Facility.FsmonkingArea, mockRoom.Facility.Ftelevison, mockRoom.Facility.FAc,
-	// 	mockRoom.Facility.Fbathroom, mockRoom.Facility.FcoffeMaker, mockRoom.Facility.CreatedAt,
-	// 	mockRoom.Facility.UpdatedAt, mockRoom.Status, mockRoom.CreatedAt, mockRoom.UpdatedAt))
-
-	// actual, err := suite.repo.GetByRoomType(mockRoom.RoomType)
-
-	// assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
-	// assert.Nil(suite.T(), err)
-	// assert.NoError(suite.T(), err)
-	// assert.Len(suite.T(), actual, 1)
-
 }
 
-func (suite *RoomRepositoryTestSuite) TestGetRoom_Success() {
+func (suite *RoomRepositoryTestSuite) TestGet_Success() {
+	mockRoom := model.Room{
+		Id:          "1",
+		RoomType:    "room test",
+		MaxCapacity: 10,
+		Facility: model.RoomFacility{
+			Id:               "101",
+			RoomDescription:  "ruangan test",
+			Fwifi:            "ada",
+			FsoundSystem:     "ada",
+			Fprojector:       "ada",
+			FscreenProjector: "ada",
+			Fchairs:          "ada",
+			Ftables:          "ada",
+			FsoundProof:      "ada",
+			FsmonkingArea:    "ada",
+			Ftelevison:       "ada",
+			FAc:              "ada",
+			Fbathroom:        "ada",
+			FcoffeMaker:      "ada",
+			CreatedAt:        time.Time{},
+			UpdatedAt:        time.Time{},
+		},
+		Status:    "available",
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	}
 
-	// expectedRoom := model.Room{
-	// 	Id:          "1",
-	// 	RoomType:    "test",
-	// 	MaxCapacity: 10,
-	// 	Facility: model.RoomFacility{
-	// 		Id:               "1",
-	// 		RoomDescription:  "ruangan test",
-	// 		Fwifi:            "",
-	// 		FsoundSystem:     "",
-	// 		Fprojector:       "",
-	// 		FscreenProjector: "",
-	// 		Fchairs:          "",
-	// 		Ftables:          "",
-	// 		FsoundProof:      "",
-	// 		FsmonkingArea:    "",
-	// 		Ftelevison:       "",
-	// 		FAc:              "",
-	// 		Fbathroom:        "",
-	// 		FcoffeMaker:      "",
-	// 		CreatedAt:        time.Time{},
-	// 		UpdatedAt:        time.Time{},
-	// 	},
-	// 	Status:    "available",
-	// 	CreatedAt: time.Time{},
-	// 	UpdatedAt: time.Time{},
-	// }
+	rows := sqlmock.NewRows([]string{
+		"id", "roomtype", "capacity",
+		"facilityid", "roomdescription", "fwifi", "fsoundsystem", "fprojector", "fscreenprojector", "fchairs", "ftables", "fsoundproof", "fsmonkingarea", "ftelevison", "fac", "fbathroom", "fcoffemaker", "updatedat", "createdat",
+		"status", "createdat", "updatedat",
+	}).
+		AddRow(
+			mockRoom.Id,
+			mockRoom.RoomType,
+			mockRoom.MaxCapacity,
+			mockRoom.Facility.Id,
+			mockRoom.Facility.RoomDescription,
+			mockRoom.Facility.Fwifi,
+			mockRoom.Facility.FsoundSystem,
+			mockRoom.Facility.Fprojector,
+			mockRoom.Facility.FscreenProjector,
+			mockRoom.Facility.Fchairs,
+			mockRoom.Facility.Ftables,
+			mockRoom.Facility.FsoundProof,
+			mockRoom.Facility.FsmonkingArea,
+			mockRoom.Facility.Ftelevison,
+			mockRoom.Facility.FAc,
+			mockRoom.Facility.Fbathroom,
+			mockRoom.Facility.FcoffeMaker,
+			mockRoom.Facility.UpdatedAt,
+			mockRoom.Facility.CreatedAt,
+			mockRoom.Status,
+			mockRoom.CreatedAt,
+			mockRoom.UpdatedAt,
+		)
 
-	// rows := sqlmock.NewRows([]string{"id", "roomtype", "capacity", "id", "roomdescription", "fwifi", "fsoundsystem", "fprojector", "fscreenprojector", "fchairs", "ftables", "fsoundproof", "fsmonkingarea", "ftelevison", "fac", "fbathroom", "fcoffemaker", "createdat", "updatedat", "status", "createdat", "updatedat"}).
-	// 	AddRow(expectedRoom.Id, expectedRoom.RoomType, expectedRoom.MaxCapacity,
-	// 		expectedRoom.Facility.Id, expectedRoom.Facility.RoomDescription, expectedRoom.Facility.Fwifi,
-	// 		expectedRoom.Facility.FsoundSystem, expectedRoom.Facility.Fprojector, expectedRoom.Facility.FscreenProjector,
-	// 		expectedRoom.Facility.Fchairs, expectedRoom.Facility.Ftables, expectedRoom.Facility.FsoundProof,
-	// 		expectedRoom.Facility.FsmonkingArea, expectedRoom.Facility.Ftelevison, expectedRoom.Facility.FAc,
-	// 		expectedRoom.Facility.Fbathroom, expectedRoom.Facility.FcoffeMaker, expectedRoom.Facility.CreatedAt,
-	// 		expectedRoom.Facility.UpdatedAt, expectedRoom.Status, expectedRoom.CreatedAt, expectedRoom.UpdatedAt)
+	suite.mockSql.ExpectQuery(`SELECT r.id, r.roomtype, r.capacity, f.id, f.roomdescription, f.fwifi, f.fsoundsystem, f.fprojector, f.fscreenprojector, f.fchairs, f.ftables, f.fsoundproof, f.fsmonkingarea, f.ftelevison, f.fac, f.fbathroom, f.fcoffemaker, f.createdat, f.updatedat, r.status, r.createdat, r.updatedat FROM rooms AS r JOIN facilities AS f ON f.id = r.facilities WHERE r.id = \$1;`).
+		WithArgs("1").
+		WillReturnRows(rows)
 
+	result, err := suite.repo.Get("1")
+
+	assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
+	assert.Nil(suite.T(), err)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), mockRoom, result)
+}
+
+func (suite *RoomRepositoryTestSuite) TestGetAllRoomByStatus_Success() {
+	mockRoom := []model.Room{
+		{
+			Id:          "1",
+			RoomType:    "room test",
+			MaxCapacity: 10,
+			Facility: model.RoomFacility{
+				Id:               "101",
+				RoomDescription:  "ruangan test",
+				Fwifi:            "ada",
+				FsoundSystem:     "ada",
+				Fprojector:       "ada",
+				FscreenProjector: "ada",
+				Fchairs:          "ada",
+				Ftables:          "ada",
+				FsoundProof:      "ada",
+				FsmonkingArea:    "ada",
+				Ftelevison:       "ada",
+				FAc:              "ada",
+				Fbathroom:        "ada",
+				FcoffeMaker:      "ada",
+				CreatedAt:        time.Time{},
+				UpdatedAt:        time.Time{},
+			},
+			Status:    "available",
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		},
+	}
+	rows := sqlmock.NewRows([]string{
+		"id", "roomtype", "capacity",
+		"facilityid", "roomdescription", "fwifi", "fsoundsystem", "fprojector", "fscreenprojector", "fchairs", "ftables", "fsoundproof", "fsmonkingarea", "ftelevison", "fac", "fbathroom", "fcoffemaker", "updatedat", "createdat",
+		"status", "createdat", "updatedat",
+	}).
+		AddRow(
+			mockRoom[0].Id,
+			mockRoom[0].RoomType,
+			mockRoom[0].MaxCapacity,
+			mockRoom[0].Facility.Id,
+			mockRoom[0].Facility.RoomDescription,
+			mockRoom[0].Facility.Fwifi,
+			mockRoom[0].Facility.FsoundSystem,
+			mockRoom[0].Facility.Fprojector,
+			mockRoom[0].Facility.FscreenProjector,
+			mockRoom[0].Facility.Fchairs,
+			mockRoom[0].Facility.Ftables,
+			mockRoom[0].Facility.FsoundProof,
+			mockRoom[0].Facility.FsmonkingArea,
+			mockRoom[0].Facility.Ftelevison,
+			mockRoom[0].Facility.FAc,
+			mockRoom[0].Facility.Fbathroom,
+			mockRoom[0].Facility.FcoffeMaker,
+			mockRoom[0].Facility.UpdatedAt,
+			mockRoom[0].Facility.CreatedAt,
+			mockRoom[0].Status,
+			mockRoom[0].CreatedAt,
+			mockRoom[0].UpdatedAt,
+		)
+
+	suite.mockSql.ExpectQuery(`SELECT r.id, r.roomtype, r.capacity, f.id, f.roomdescription, f.fwifi, f.fsoundsystem, f.fprojector, f.fscreenprojector, f.fchairs, f.ftables, f.fsoundproof, f.fsmonkingarea, f.ftelevison, f.fac, f.fbathroom, f.fcoffemaker, f.createdat, f.updatedat, r.status, r.createdat, r.updatedat FROM rooms AS r JOIN facilities AS f ON f.id = r.facilities WHERE r.status = \$1`).
+		WithArgs("available").
+		WillReturnRows(rows)
+
+	result, err := suite.repo.GetAllRoomByStatus("available")
+
+	assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
+	assert.Nil(suite.T(), err)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), mockRoom, result)
+}
+
+func (suite *RoomRepositoryTestSuite) TestGetAllRoom() {
+	mockRoom := []model.Room{
+		{
+			Id:          "1",
+			RoomType:    "room test",
+			MaxCapacity: 10,
+			Facility: model.RoomFacility{
+				Id:               "101",
+				RoomDescription:  "ruangan test",
+				Fwifi:            "ada",
+				FsoundSystem:     "ada",
+				Fprojector:       "ada",
+				FscreenProjector: "ada",
+				Fchairs:          "ada",
+				Ftables:          "ada",
+				FsoundProof:      "ada",
+				FsmonkingArea:    "ada",
+				Ftelevison:       "ada",
+				FAc:              "ada",
+				Fbathroom:        "ada",
+				FcoffeMaker:      "ada",
+				CreatedAt:        time.Time{},
+				UpdatedAt:        time.Time{},
+			},
+			Status:    "available",
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		},
+	}
+
+	rows := sqlmock.NewRows([]string{
+		"id", "roomtype", "capacity",
+		"facilityid", "roomdescription", "fwifi", "fsoundsystem", "fprojector", "fscreenprojector", "fchairs", "ftables", "fsoundproof", "fsmonkingarea", "ftelevison", "fac", "fbathroom", "fcoffemaker", "updatedat", "createdat",
+		"status", "createdat", "updatedat",
+	}).
+		AddRow(
+			mockRoom[0].Id,
+			mockRoom[0].RoomType,
+			mockRoom[0].MaxCapacity,
+			mockRoom[0].Facility.Id,
+			mockRoom[0].Facility.RoomDescription,
+			mockRoom[0].Facility.Fwifi,
+			mockRoom[0].Facility.FsoundSystem,
+			mockRoom[0].Facility.Fprojector,
+			mockRoom[0].Facility.FscreenProjector,
+			mockRoom[0].Facility.Fchairs,
+			mockRoom[0].Facility.Ftables,
+			mockRoom[0].Facility.FsoundProof,
+			mockRoom[0].Facility.FsmonkingArea,
+			mockRoom[0].Facility.Ftelevison,
+			mockRoom[0].Facility.FAc,
+			mockRoom[0].Facility.Fbathroom,
+			mockRoom[0].Facility.FcoffeMaker,
+			mockRoom[0].Facility.UpdatedAt,
+			mockRoom[0].Facility.CreatedAt,
+			mockRoom[0].Status,
+			mockRoom[0].CreatedAt,
+			mockRoom[0].UpdatedAt,
+		)
+
+	suite.mockSql.ExpectQuery(`SELECT r.id, r.roomtype, r.capacity, f.id, f.roomdescription, f.fwifi, f.fsoundsystem, f.fprojector, f.fscreenprojector, f.fchairs, f.ftables, f.fsoundproof, f.fsmonkingarea, f.ftelevison, f.fac, f.fbathroom, f.fcoffemaker, f.createdat, f.updatedat, r.status, r.createdat, r.updatedat FROM rooms AS r JOIN facilities AS f ON f.id = r.facilities`).
+		WillReturnRows(rows)
+
+	result, err := suite.repo.GetAllRoom()
+
+	assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
+	assert.Nil(suite.T(), err)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), mockRoom, result)
+}
+
+func (suite *RoomRepositoryTestSuite) TestChangeStatus_Success() {
+	roomId := "110"
+
+	suite.mockSql.ExpectExec(`UPDATE rooms SET status = \$1 WHERE id = \$2`).
+		WithArgs("available", roomId).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := suite.repo.ChangeStatus(roomId)
+
+	assert.NoError(suite.T(), suite.mockSql.ExpectationsWereMet())
+	assert.Nil(suite.T(), err)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), roomId, roomId)
 }
--- usecase/room_usecase_test.go
+++ usecase/room_usecase_test.go
@@ -1,13 +1,15 @@
 package usecase
 
 import (
-	"final-project-booking-room/model"
+	"fmt"
+	"project-final/model"
 	repositorymock "project-final/unit-test/mock-test/repository-mock"
 
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -118,4 +120,72 @@ func (suite *RoomUsecaseTestSuite) TestGetAllRoomByStatus() {
 	_, err := suite.ru.GetAllRoomByStatus(mockRoom.Status)
 	assert.Nil(suite.T(), err)
 	assert.NoError(suite.T(), err)
+}
+
+func (suite *RoomUsecaseTestSuite) TestDeleteById() {
+	expectedError := fmt.Errorf("room with id %s not found", mockRoom.Id)
+
+	suite.rrm.On("Delete", mockRoom.Id).Return(model.Room{}, expectedError)
+
+	deletedRoom, err := suite.ru.DeleteById(mockRoom.Id)
+
+	suite.rrm.AssertExpectations(suite.T())
+
+	assert.EqualError(suite.T(), err, expectedError.Error())
+	assert.Equal(suite.T(), model.Room{}, deletedRoom)
+}
+
+func (suite *RoomUsecaseTestSuite) TestFindById() {
+	expectedError := fmt.Errorf("room with ID %s not found", mockRoom.Id)
+	suite.rrm.On("Get", mockRoom.Id).Return(model.Room{}, expectedError)
+
+	foundRoom, err := suite.ru.FindById(mockRoom.Id)
+
+	suite.rrm.AssertExpectations(suite.T())
+
+	assert.EqualError(suite.T(), err, expectedError.Error())
+	assert.Equal(suite.T(), model.Room{}, foundRoom)
+}
+
+func (suite *RoomUsecaseTestSuite) TestUpdateById_Success() {
+	roomId := "1"
+	var updatePayload = model.Room{
+		Id:          "1",
+		RoomType:    "test",
+		MaxCapacity: 10,
+		Facility: model.RoomFacility{
+			Id:               "1",
+			RoomDescription:  "ruangan test",
+			Fwifi:            "ada",
+			FsoundSystem:     "ada",
+			Fprojector:       "ada",
+			FscreenProjector: "ada",
+			Fchairs:          "ada",
+			Ftables:          "ada",
+			FsoundProof:      "ada",
+			FsmonkingArea:    "ada",
+			Ftelevison:       "ada",
+			FAc:              "ada",
+			Fbathroom:        "ada",
+			FcoffeMaker:      "ada",
+			CreatedAt:        time.Time{},
+			UpdatedAt:        time.Time{},
+		},
+		Status:    "available",
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	}
+
+	expectedError := fmt.Errorf("room with id %s not found", updatePayload.Id)
+
+	suite.rrm.On("Get", mockRoom.Id).Return(mockRoom, nil)
+
+	suite.rrm.On("Update", roomId, mock.AnythingOfType("model.Room")).Return(model.Room{}, expectedError)
+
+	updatedRoom, err := suite.ru.UpdateById(roomId, updatePayload)
+
+	suite.rrm.AssertExpectations(suite.T())
+
+	assert.EqualError(suite.T(), err, expectedError.Error())
+	assert.Equal(suite.T(), model.Room{}, updatedRoom)
 }
